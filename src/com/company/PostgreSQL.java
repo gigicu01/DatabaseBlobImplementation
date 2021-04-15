711b3512a0f7b8e33f425b6520db9f3079d8e953
sfarian22 <81589615+sfarian22@users.noreply.github.com>
2021-04-15 11:16:38 -0400

    Update PostgreSQL.java

--- src/com/company/PostgreSQL.java
+++ src/com/company/PostgreSQL.java
@@ -10,8 +10,8 @@ public class PostgreSQL {
         try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/testdb",
-                            "postgres", "123");
+                    .getConnection("jdbc:postgresql://localhost:5432/CompanyCoPayroll",
+                            "companyCo", "MacroHard");
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName()+": "+e.getMessage());
