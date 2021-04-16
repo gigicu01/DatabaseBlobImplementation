45eb5682076e8329bc9f42044012f1059f816e75
bmeyers23 <81589613+bmeyers23@users.noreply.github.com>
2021-04-16 13:00:00 -0400

    MySQL

---
+++ src/LoadDriver.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class LoadDriver {
+    public static void main(String[] args) {
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception ex) {
+            // handle the error
+        }
+    }
+}
