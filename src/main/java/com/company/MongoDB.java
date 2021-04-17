16923dc5923361a658d22859ebeab18c732931db
gigicu01 <80425896+gigicu01@users.noreply.github.com>
2021-04-16 20:44:54 -0400

    sdsds

--- src/main/java/com/company/MongoDB.java
+++ src/main/java/com/company/MongoDB.java
@@ -1,5 +1,6 @@
 package com.company;
 
+
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
 import com.mongodb.*;
@@ -14,8 +15,11 @@ import java.util.Arrays;
 public class MongoDB {
 
     public void connectMongoDB() {
-        MongoClient mongoClient = MongoClients.create(
-                "mongodb+srv://gigicu01:<Nidodage01>@cluster0.5i6vl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-        MongoDatabase database = mongoClient.getDatabase("test");
+        try {
+            MongoClient mongoClient = MongoClients.create(
+                    "mongodb+srv://gigicu01:<password>@cluster0.5i6vl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+            MongoDatabase database = mongoClient.getDatabase("test");
+        }
+        catch(Exception e){}
     }
 }
